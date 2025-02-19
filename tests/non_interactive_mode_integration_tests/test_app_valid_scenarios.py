0c29b1b7ff2f6050b63117d42fe3f3b0d87ced0d
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-02-19 14:41:53 +0100

    Add tests

--- tests/non_interactive_mode_integration_tests/test_app_valid_scenarios.py
+++ tests/non_interactive_mode_integration_tests/test_app_valid_scenarios.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 from time import sleep
 from test_utils.test_utils import (
     run_package_to_image_placer,
@@ -28,10 +29,124 @@ def test_write_one_package(package_to_image_placer_binary):
     partitions = [1]
 
     create_test_package(package, "10KB")
-    create_image(img_in, "10MB", 2)
+    create_image(img_in, "10MB", 1)
     make_image_mountable(img_in)
 
     create_config(config, img_in, img_out, [package_zip], partitions)
+
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+
+    assert result.returncode == 0
+
+    assert inspect_image(config)
+
+
+def test_write_two_packages(package_to_image_placer_binary):
+    """TODO"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out = "test_data/test_img_out.img"
+    package_1 = "test_data/normal_package_1"
+    package_2 = "test_data/normal_package_2"
+    partitions = [1]
+
+    create_test_package(package_1, "15KB")
+    create_test_package(package_2, "5KB")
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
+
+    create_config(config, img_in, img_out, [package_1 + ".zip", package_2 + ".zip"], partitions)
+
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+
+    assert result.returncode == 0
+
+    assert inspect_image(config)
+
+
+def test_write_multiple_packages_to_multiple_partitions(package_to_image_placer_binary):
+    """TODO"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out = "test_data/test_img_out.img"
+
+    package_1 = "test_data/normal_package1"
+    package_2 = "test_data/normal_package2"
+    package_3 = "test_data/normal_package3"
+    package_4 = "test_data/normal_package4"
+    package_5 = "test_data/normal_package5"
+
+    partitions = [1, 2, 3, 4]
+
+    create_test_package(package_1, "10KB")
+    create_test_package(package_2, "10KB")
+    create_test_package(package_3, "10KB")
+    create_test_package(package_4, "10KB")
+    create_test_package(package_5, "10KB")
+
+    create_image(img_in, "10MB", 5)
+    make_image_mountable(img_in)
+
+    create_config(
+        config,
+        img_in,
+        img_out,
+        [package_1 + ".zip", package_2 + ".zip", package_3 + ".zip", package_4 + ".zip", package_5 + ".zip"],
+        partitions,
+    )
+
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+
+    assert result.returncode == 0
+
+    assert inspect_image(config)
+
+
+def test_write_one_package_target_override(package_to_image_placer_binary):
+    """TODO"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out = "test_data/test_img_out.img"
+    package = "test_data/normal_package"
+    package_zip = package + ".zip"
+    partitions = [1]
+
+    create_test_package(package, "10KB")
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
+
+    with open(img_out, "w") as f:
+        f.write("Test data")
+
+    sleep(5)
+
+    create_config(config, img_in, img_out, [package_zip], partitions, overwrite=False)
+
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+
+    assert result.returncode == 0
+
+    assert inspect_image(config)
+
+    # sleep(100)
+
+
+def test_write_one_package_no_clone(package_to_image_placer_binary):
+    """FIXME"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out = "test_data/test_img_out.img"
+    package = "test_data/normal_package"
+    package_zip = package + ".zip"
+    partitions = [1]
+
+    create_test_package(package, "10KB")
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
+
+    create_config(
+        config, source=img_in, target=img_out, packages=[package_zip], partition_numbers=partitions, no_clone=True
+    )
 
     result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
 
