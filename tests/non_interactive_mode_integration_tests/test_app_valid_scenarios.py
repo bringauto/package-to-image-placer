219dc1e4836ddc26f11064a5120101c98aca08e5
Ondrej Vala <93932164+Mayt34@users.noreply.github.com>
2025-02-20 09:55:05 +0100

    Add another tests

--- tests/non_interactive_mode_integration_tests/test_app_valid_scenarios.py
+++ tests/non_interactive_mode_integration_tests/test_app_valid_scenarios.py
@@ -118,8 +118,6 @@ def test_write_one_package_target_override(package_to_image_placer_binary):
     with open(img_out, "w") as f:
         f.write("Test data")
 
-    sleep(5)
-
     create_config(config, img_in, img_out, [package_zip], partitions, overwrite=False)
 
     result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
@@ -132,7 +130,7 @@ def test_write_one_package_target_override(package_to_image_placer_binary):
 
 
 def test_write_one_package_no_clone(package_to_image_placer_binary):
-    """FIXME"""
+    """TODO"""
     config = "test_data/test_config.json"
     img_in_out = "test_data/test_img.img.in"
     package = "test_data/normal_package"
@@ -149,83 +147,66 @@ def test_write_one_package_no_clone(package_to_image_placer_binary):
 
     assert result.returncode == 0
 
-    # assert inspect_image(config)
+    assert inspect_image(config)
 
 
-# def test_app_finishes(target_disk_setup_binary):
-#     """Test that the target disk setup application completes successfully when provided with valid arguments."""
+def test_write_one_package_overwrite_config_value_to_true(package_to_image_placer_binary):
+    """TODO"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out_1 = "test_data/test_img_out_1.img"
+    img_out_2 = "test_data/test_img_out_2.img"
+    package = "test_data/normal_package"
+    package_zip = package + ".zip"
+    partitions = [1]
 
-#     bootloader = create_disk_image("test_data/bootloader.img", "100M", "ext4")
-#     rootfs = create_disk_image("test_data/rootfs.img", "100M", "ext4")
-#     recovery = create_disk_image("test_data/recovery.img", "100M", "ext4")
-#     device = create_disk_image("test_data/sd32.img", "32G", "vfat")
-#     loop_device = make_image_mountable(device)
+    create_test_package(package, "10KB")
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
 
-#     result = run_target_disk_setup(
-#         target_disk_setup_binary, bootloader, rootfs, loop_device, recovery=recovery, send_to_stdin="y"
-#     )
+    create_config(config, img_in, img_out_1, [package_zip], partitions)
 
-#     assert result.returncode == 0
+    # write package to the image for the first time
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+    assert result.returncode == 0
+    assert inspect_image(config)
 
+    # write package to the image for the second time (it should fail because override is not set)
+    create_config(config, img_out_1, img_out_2, [package_zip], partitions)
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+    assert result.returncode == 1
+    assert not os.path.exists(img_out_2)
 
-# def test_app_writes_to_disk(target_disk_setup_binary):
-#     """Test that the target disk setup application completes successfully when provided with valid arguments (bootloader, rootfs) and checks if the data was written to the disk correctly."""
-
-#     bootloader = create_disk_image("test_data/bootloader.img", "10M", "ext4")
-#     rootfs = create_disk_image("test_data/rootfs.img", "50M", "ext4")
-#     fill_disk_images_with_data([bootloader, rootfs])
-
-#     device = create_disk_image("test_data/sd32.img", "32G", "vfat")
-#     loop_device = make_image_mountable(device)
-
-#     result = run_target_disk_setup(target_disk_setup_binary, bootloader, rootfs, loop_device, send_to_stdin="y")
-
-#     assert result.returncode == 0
-
-#     assert inspect_device(loop_device, bootloader, rootfs)
-
-
-# def test_app_writes_to_disk_with_recovery(target_disk_setup_binary):
-#     """Test that the target disk setup application completes successfully when provided with valid arguments (bootloader, rootfs, recovery) and checks if the data was written to the disk correctly."""
-
-#     bootloader = create_disk_image("test_data/bootloader.img", "15M", "ext4")
-#     rootfs = create_disk_image("test_data/rootfs.img", "60M", "ext4")
-#     recovery = create_disk_image("test_data/recovery.img", "50M", "ext4")
-#     fill_disk_images_with_data([bootloader, rootfs, recovery])
-
-#     device = create_disk_image("test_data/sd32.img", "32G", "vfat")
-#     loop_device = make_image_mountable(device)
-
-#     result = run_target_disk_setup(
-#         target_disk_setup_binary, bootloader, rootfs, loop_device, recovery=recovery, send_to_stdin="y"
-#     )
-
-#     assert result.returncode == 0
-
-#     assert inspect_device(loop_device, bootloader, rootfs, recovery=recovery)
+    # write package to the image for the second time (it should pass because override is set)
+    create_config(config, img_out_1, img_out_2, [package_zip], partitions)
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config, overwrite=True)
+    assert result.returncode == 0
+    assert inspect_image(config)
 
 
-# def test_app_writes_to_disk_with_log_folder(target_disk_setup_binary):
-#     """Test that the target disk setup application completes successfully when provided with valid arguments (bootloader, rootfs,v log_folder), checks if the data was written to the disk correctly and if non-empty log file was created."""
+def test_write_one_package_overwrite_config_value_to_false(package_to_image_placer_binary):
+    """TODO"""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out_1 = "test_data/test_img_out_1.img"
+    img_out_2 = "test_data/test_img_out_2.img"
+    package = "test_data/normal_package"
+    package_zip = package + ".zip"
+    partitions = [1]
 
-#     bootloader = create_disk_image("test_data/bootloader.img", "15M", "ext4")
-#     rootfs = create_disk_image("test_data/rootfs.img", "60M", "ext4")
-#     log_folder = "test_data"
-#     fill_disk_images_with_data([bootloader, rootfs])
+    create_test_package(package, "10KB")
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
 
-#     device = create_disk_image("test_data/sd32.img", "32G", "vfat")
-#     loop_device = make_image_mountable(device)
+    create_config(config, img_in, img_out_1, [package_zip], partitions)
 
-#     result = run_target_disk_setup(
-#         target_disk_setup_binary, bootloader, rootfs, loop_device, log_folder=log_folder, send_to_stdin="y"
-#     )
+    # write package to the image for the first time
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+    assert result.returncode == 0
+    assert inspect_image(config)
 
-#     log_file_path = f"{log_folder}/target_disk_setup.log"
-#     assert os.path.exists(log_file_path)
-#     # Check if the log file is not empty
-#     with open(log_file_path, "r") as log_file:
-#         assert log_file.read().strip() != ""
-
-#     assert result.returncode == 0
-
-#     assert inspect_device(loop_device, bootloader, rootfs)
+    # write package to the image for the second time (it should fail because override is set to false from arguments and it should overshadow the value from the config file)
+    create_config(config, img_out_1, img_out_2, [package_zip], partitions, overwrite=True)
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config, overwrite=False)
+    assert result.returncode == 1
+    assert not os.path.exists(img_out_2)
