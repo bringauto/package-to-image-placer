ed43b61880389a9faa0f510c1a2938d0a79fe59c
Ondrej Vala <93932164+ondrejvala2@users.noreply.github.com>
2025-04-23 14:39:48 +0200

    Update services integration tests

--- tests/non_interactive_mode_integration_tests/test_app_service_scenarios.py
+++ tests/non_interactive_mode_integration_tests/test_app_service_scenarios.py
@@ -68,14 +68,17 @@ def test_03_write_packages_with_services_with_override(package_to_image_placer_b
     img_out = "test_data/test_img_out.img"
     package1 = "test_data/normal_package"
     package2 = "test_data/normal_package2"
+    package3 = "test_data/normal_package3"
     package1_zip = package1 + ".zip"
     package2_zip = package2 + ".zip"
+    package3_zip = package3 + ".zip"
     service1 = "test_data/service1.service"
     partitions = [1, 3]
 
     create_service_file(service1)
     create_test_package(package1, "10KB", services=[service1])
     create_test_package(package2, "10KB", services=[service1])
+    create_test_package(package3, "10KB", services=[service1])
     create_image(img_in, "10MB", 3)
     make_image_mountable(img_in)
 
@@ -86,6 +89,7 @@ def test_03_write_packages_with_services_with_override(package_to_image_placer_b
         [
             create_normal_package_config(package1_zip, True),
             create_normal_package_config(package2_zip, True, overwrite_file=["/normal_package2/service1.service"]),
+            create_normal_package_config(package3_zip, True, service_name_suffix="test"),
         ],
         partitions,
     )
@@ -93,6 +97,7 @@ def test_03_write_packages_with_services_with_override(package_to_image_placer_b
     result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
 
     assert result.returncode == 0
+    sleep(1000)
     assert inspect_image(config)
 
 
@@ -261,6 +266,42 @@ def test_08_package_with_service_with_suffix_starting_with_hyphen(
 
     create_service_file(service)
     create_test_package(package, "10KB", services=[service])
+    create_image(img_in, "10MB", 1)
+    make_image_mountable(img_in)
+
+    create_config(
+        config,
+        img_in,
+        img_out,
+        [
+            create_normal_package_config(
+                package_zip,
+                True,
+                service_name_suffix="-test",
+            )
+        ],
+        partitions,
+    )
+
+    result = run_package_to_image_placer(package_to_image_placer_binary, config=config)
+
+    assert result.returncode == 1
+
+    assert not os.path.exists(img_out)
+
+
+def test_09_try_to_write_service_when_package_do_not_contain_any_service(
+    package_to_image_placer_binary,
+):
+    """Creates package without service file and try to write it with service. It should crash."""
+    config = "test_data/test_config.json"
+    img_in = "test_data/test_img.img.in"
+    img_out = "test_data/test_img_out.img"
+    package = "test_data/normal_package"
+    package_zip = package + ".zip"
+    partitions = [1]
+
+    create_test_package(package, "10KB")
     create_image(img_in, "10MB", 1)
     make_image_mountable(img_in)
 
